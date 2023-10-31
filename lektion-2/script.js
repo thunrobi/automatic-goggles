5f3a2cf75c8f72babdfb92ca5a82c5562882e4d1
Robin <thunrobi@arcada.fi>
2023-10-31 12:01:25 +0200

    Add files for lesson 2

---
+++ lektion-2/script.js
@@ -0,0 +1,15 @@
+const namn = "JavaScript";
+const version = 1.0;
+
+const str1 = "Hello "+ namn + " version " + version + "!";
+
+function hello(){
+const str2 = `Hello function!`;
+console.log(str2);
+}
+function btnHandler(){
+    console.log("Du tryckte på knappen 57!")
+}
+hello();
+
+console.log(str1);
